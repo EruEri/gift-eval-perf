972847afca5b23dd3c384e1cfc42c20b24b1c48e
nayu <ndiayeyves@gmail.com>
2025-02-07 13:15:40 +0100

    Assert: 	- If block is different than the expect cipher write time = 0

--- stm32f407vg/src/main.c
+++ stm32f407vg/src/main.c
@@ -43,7 +43,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-volatile unsigned long bench_cycles[] = {-1, -1, -1};
+volatile unsigned long bench_cycles[3] = {-1, -1, -1};
 
 const u8 key_gift[KEY_SIZE] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 1st key
@@ -53,6 +53,11 @@ const u8 block0[GIFT64_BLOCK_SIZE] = {0x00, 0x00, 0x00, 0x00,
                                       0x00, 0x00, 0x00, 0x00}; // 1st plaintext
 const u8 block1[GIFT64_BLOCK_SIZE] = {0x00, 0x00, 0x00, 0x00,
                                       0x00, 0x00, 0x00, 0x00};
+
+const u8 expect_ciphertext0[GIFT64_BLOCK_SIZE * 2] = {
+    // f6 2b c3 ef 34 f7 75 ac
+    0xf6, 0x2b, 0xc3, 0xef, 0x34, 0xf7, 0x75, 0xac,
+    0xf6, 0x2b, 0xc3, 0xef, 0x34, 0xf7, 0x75, 0xac};
 
 // const u8 key_gift[KEY_SIZE] = {0xbd, 0x91, 0x73, 0x1e, 0xb6, 0xbc, 0x27,
 // 0x13,
@@ -88,41 +93,41 @@ u8 ciphertexts[GIFT64_BLOCK_SIZE * 2] = {0};
  */
 
 static unsigned long get_cycle_count() {
-  /* retrieve the CPU cycles using the Cortex-M DWT->CYCCNT register
-   * avaialable only starting from CM3
-   */
+    /* retrieve the CPU cycles using the Cortex-M DWT->CYCCNT register
+     * avaialable only starting from CM3
+     */
 
 #if (__CORTEX_M >= 0x03U)
-  static int dwt_started = 0;
-  if (dwt_started == 0) {
-    dwt_started = 1;
-    /* Enable Tracing */
-    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    static int dwt_started = 0;
+    if (dwt_started == 0) {
+        dwt_started = 1;
+        /* Enable Tracing */
+        CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
 #if (__CORTEX_M == 0x07U)
-    /* in Cortex M7, the trace needs to be unlocked
-     * via the DWT->LAR register with 0xC5ACCE55 value
-     */
-    DWT->LAR = 0xC5ACCE55;
+        /* in Cortex M7, the trace needs to be unlocked
+         * via the DWT->LAR register with 0xC5ACCE55 value
+         */
+        DWT->LAR = 0xC5ACCE55;
 #endif
-    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+        DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 
-    /* Reset counter */
-    DWT->CYCCNT = 0;
-  }
+        /* Reset counter */
+        DWT->CYCCNT = 0;
+    }
 
-  return (unsigned long)DWT->CYCCNT;
+    return (unsigned long)DWT->CYCCNT;
 #else
-  return 0;
+    return 0;
 #endif
 }
 
 unsigned long gift() {
-  u32 rkeys[56] = {0};
-  gift64_rearrange_key(rkeys, key_gift);
-  giftb64_keyschedule(rkeys);
-  unsigned long t1 = get_cycle_count();
-  gift64_encrypt_block(ciphertexts, rkeys, block0, block1);
-  return get_cycle_count() - t1;
+    u32 rkeys[56] = {0};
+    gift64_rearrange_key(rkeys, key_gift);
+    giftb64_keyschedule(rkeys);
+    unsigned long t1 = get_cycle_count();
+    gift64_encrypt_block(ciphertexts, rkeys, block0, block1);
+    return get_cycle_count() - t1;
 }
 
 static void SystemClock_Config(void);
@@ -150,42 +155,43 @@ static unsigned long get_cycle_count();
  * @retval None
  */
 static void SystemClock_Config(void) {
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
 
-  /* Enable Power Control clock */
-  __HAL_RCC_PWR_CLK_ENABLE();
+    /* Enable Power Control clock */
+    __HAL_RCC_PWR_CLK_ENABLE();
 
-  /* The voltage scaling allows optimizing the power consumption when the device
-     is clocked below the maximum system frequency, to update the voltage
-     scaling value regarding system frequency refer to product datasheet.  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+    /* The voltage scaling allows optimizing the power consumption when the
+       device is clocked below the maximum system frequency, to update the
+       voltage scaling value regarding system frequency refer to product
+       datasheet.  */
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-  /* Enable HSI Oscillator and activate PLL with HSI as source */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 0x10;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-    Error_Handler();
-  }
+    /* Enable HSI Oscillator and activate PLL with HSI as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = 0x10;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 16;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
-     clocks dividers */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
-                                 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
-    Error_Handler();
-  }
+    /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+       clocks dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+                                   RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /**
@@ -194,10 +200,10 @@ static void SystemClock_Config(void) {
  * @retval None
  */
 static void Error_Handler(void) {
-  /* Turn LED2 on */
-  BSP_LED_On(LED2);
-  while (1) {
-  }
+    /* Turn LED2 on */
+    BSP_LED_On(LED2);
+    while (1) {
+    }
 }
 
 #ifdef USE_FULL_ASSERT
@@ -209,13 +215,13 @@ static void Error_Handler(void) {
  * @retval None
  */
 void assert_failed(uint8_t *file, uint32_t line) {
-  /* User can add his own implementation to report the file name and line
-     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
-     line) */
+    /* User can add his own implementation to report the file name and line
+       number, ex: printf("Wrong parameters value: file %s on line %d\r\n",
+       file, line) */
 
-  /* Infinite loop */
-  while (1) {
-  }
+    /* Infinite loop */
+    while (1) {
+    }
 }
 #endif
 
@@ -231,30 +237,44 @@ void assert_failed(uint8_t *file, uint32_t line) {
 
 /* Private functions ---------------------------------------------------------*/
 
+// Returns 0 if [lhs] != [rhs] for the n bytes
+int memeq(const void *lhs, const void *rhs, size_t n) {
+    for (size_t i = 0; i < n; i += 1) {
+        if (((char *)lhs)[i] != ((char *)rhs)[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
 /**
  * @brief  Main program
  * @param  None
  * @retval None
  */
 int main(void) {
-  /* STM32F4xx HAL library initialization:
-       - Configure the Flash prefetch, instruction and Data caches
-       - Configure the Systick to generate an interrupt each 1 msec
-       - Set NVIC Group Priority to 4
-       - Global MSP (MCU Support Package) initialization
-     */
-  HAL_Init();
+    /* STM32F4xx HAL library initialization:
+         - Configure the Flash prefetch, instruction and Data caches
+         - Configure the Systick to generate an interrupt each 1 msec
+         - Set NVIC Group Priority to 4
+         - Global MSP (MCU Support Package) initialization
+       */
+    HAL_Init();
 
-  /* Configure the system clock to 84 MHz */
-  SystemClock_Config();
+    /* Configure the system clock to 84 MHz */
+    SystemClock_Config();
 
-  for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) {
 
-    // bench_lens[i] = len;
-    unsigned long cycles = gift();
-    bench_cycles[i] = cycles;
-  }
+        // bench_lens[i] = len;
+        unsigned long cycles = gift();
+        bench_cycles[i] = cycles;
+        if (!memeq(ciphertexts, expect_ciphertext0, GIFT64_BLOCK_SIZE * 2)) {
+            bench_cycles[i] = 0;
+        };
+    }
 
-  while (1) {
-  }
+    while (1) {
+    }
 }
